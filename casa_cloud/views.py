251e21064747cc92b50052a543ee4a95f712bdbb
jl237561 <mr.li.jinpeng@gmail.com>
2017-06-29 17:39:22 +0200

    [WIP] add create lock file and max num created machines

--- casa_cloud/views.py
+++ casa_cloud/views.py
@@ -2,6 +2,8 @@
 
 from datetime import datetime
 from dateutil.parser import parse
+import os
+import json
 
 from pyramid.view import view_config
 from pyramid.security import remember
@@ -39,24 +41,56 @@ def view_home(request):
     max_cpu_cores = int(request.registry.settings["docker_container_max_cores"])
     max_memory = int(request.registry.settings["docker_container_max_memory"])
     min_days_to_use = int(request.registry.settings["min_days_to_use"])
+    docker_container_create_lock_file = request.registry.settings["docker_container_create_lock_file"]
+    docker_container_max_num_containers = int(request.registry.settings["docker_container_max_num_containers"])
+    can_add_machine = True 
+
+    is_lock_create_machine = False
+    if os.path.isfile(docker_container_create_lock_file):
+        lock_data = json.load(open(docker_container_create_lock_file, "r"))
+        lock_time = lock_data["lock_time"]
+        lock_time = parse(lock_time)
+        diff_time = datetime.now() - lock_time
+        if diff_time.seconds < 10*60:
+            is_lock_create_machine = True
+
+
+    names, values = machines.search_machines(login)
+    if len(values) >= docker_container_max_num_containers:
+       can_add_machine = False
+
+
     if request.POST:
        if "del_machine_port" in request.POST:
            del_machine_port = request.POST["del_machine_port"]
            machines.remove_machine(login, int(del_machine_port))
        else:
-           cpu_cores = request.POST["cpu_cores"]
-           memory = request.POST["memory"]
-           expiry_date = parse(request.POST["expiry_date"])
-           diff_time = datetime.now() - expiry_date
-           if diff_time.days < min_days_to_use:
-               machines.create_machine(login, cpu_cores, memory, expiry_date)
-           else:
+           if is_success and is_lock_create_machine:
                is_success = False
-               error_message = "You should use at least %d days" % min_days_to_use
+               error_message = "The container creation is locked. Please wait for a moment to retry." 
+           if is_success and not can_add_machine:
+               is_success = False
+               error_message = "You already have max number of %d machines." % docker_container_max_num_containers
+
+           if is_success:
+               lock_data = {}
+               lock_data["lock_time"] = datetime.now().isoformat()
+               json.dump(lock_data, open(docker_container_create_lock_file, "w+"))
+               cpu_cores = request.POST["cpu_cores"]
+               memory = request.POST["memory"]
+               expiry_date = parse(request.POST["expiry_date"])
+               diff_time = datetime.now() - expiry_date
+               if diff_time.days < min_days_to_use:
+                   machines.create_machine(login, cpu_cores, memory, expiry_date)
+               else:
+                   is_success = False
+                   error_message = "You should use at least %d days" % min_days_to_use
+               if os.path.isfile(docker_container_create_lock_file):
+                   os.remove(docker_container_create_lock_file)
+
 
     names, values = machines.search_machines(login)
     render_machines = []
-
     for row_values in values:
         item = {}
         for i, name in enumerate(names):
